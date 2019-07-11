68d3e07c884378a9bc6e6cd0f1d0a7c1c7be1e2f
Mikey <mhmfransman@gmail.com>
2019-07-11 18:44:06 +0200

    First commit for working on the DTT testcase
    First commit for working on the DTT testcase. Added VueJS via Vue-CLI and Vue UI. Added Typescript and planning to work with GrapQl & Axios. More info/updates coming soon...

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="bg-black text-pink-500 m-4">Dit is een Marvel API app, gebouwd door Michael Fransman</h1>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" class="m-4" />
+    <h1 class="bg-green-700 text-pink-500">Hallo dit is een test</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 })
 export default class Home extends Vue {}
 </script>
